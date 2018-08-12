0dca2e0b7aff06ab996d3cac07fc09fb65ddedd0
leiwu123 <jobspecific@hotmail.com>
2018-08-12 20:00:17 +0800

    signup function working

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 def signup(request):
-  return render(request, 'accounts/signup.html')
+  if request.method == 'POST':
+    if request.POST['password1'] == request.POST['password2']:
+      try:
+        user = User.objects.get(username=request.POST['username'])
+        return render(request, 'accounts/signup.html', {'error':'Username has already been taken'})
+      except User.DoesNotExist: 
+        user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+        auth.login(request,user)
+        return redirect('home')
+    else:
+      return render(request, 'accounts/signup.html', {'error':'Passwords must match'})
+  else:
+    return render(request, 'accounts/signup.html')
+
+  #   if request.POST['password1'] == request.POST['password2']:
+  #     user = User.objects.get(username=request.POST['username'])
+  #     if user is not None:
+  #       return render(request, 'accounts/signup.html', {'error':'Username has already been taken'})
+  #     else:
+  #       user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+  #       auth.login(request,user)
+  #       return redirect('home')
+  #   else:
+  #     return render(request, 'accounts/signup.html', {'error':'Passwords must match'})
+  # else:
+  #   return render(request, 'accounts/signup.html')
+
+    
 
 def login(request):
   return render(request, 'accounts/login.html')
